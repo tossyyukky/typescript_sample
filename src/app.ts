8c52fc4b0a9108e9baba276354d9eb2a9f9a81be
Toshiyuki Ihara <toshiyuki.ihara@net-marketing.co.jp>
2018-08-24 17:57:26 +0900

    Done: Tutorial2 C3 Chart

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,31 @@
-import {Item} from './item';
-var elem = document.getElementById('output');
-var aBook = new Item('はじめてのTypeScript',1981);
-aBook.say(elem);
+import * as c3  from 'c3';
+
+let chart = c3.generate({
+    bindto: '#chart',
+    data: {
+        type: 'donut',
+        columns: [
+            ['良い感じ', 50],
+            ['ピンとこない', 50]  
+        ]
+    },
+    donut: {
+        title: 'TypeScriptの印象は？'
+    }
+});
+
+
+setTimeout(() => {
+    chart.load({
+     columns: [
+         ['良い感じ', 30],
+         ['ピンとこない', 30],
+         ['最高', 30]
+     ]
+    });
+ }, 3000);
+ setTimeout(()=> {
+     chart.unload({
+         ids: '最高'
+     });
+ }, 5000);
